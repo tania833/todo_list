e3518674ff49a05bce2003ec5b7f3f3ef5971b7f
Tania Polishchuk <tania.polischuk1998@gmail.com>
2020-11-24 11:43:06 +0200

    test

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -20,6 +20,10 @@ export const About = () => {
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic,
         voluptatem.
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic,
+        voluptatem.
+      </p>
       <img
         src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop"
         alt="a cat wearing a face mask"
